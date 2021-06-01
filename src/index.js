762104bb5db24d917643603a7fc4b2feae6c9a1c
Antje Oltmanns <antje.oltmanns@gmail.com>
2021-06-01 19:23:03 +0200

    defining, listing and creating tracks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
-const requireAuth = require('./middlewares/requireAuth')
+const requireAuth = require('./middlewares/requireAuth');
+const trackRoutes = require('./routes/trackRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:anna3000@cluster0.2myb0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
